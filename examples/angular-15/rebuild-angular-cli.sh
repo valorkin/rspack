e74c0ce95f0206d68e58fa33445e7c42d6baadc9
Dmitriy Shekhovtsovvalorkin <valorkin@gmail.com>
2023-05-18 02:43:41 +0200

    chore: full compilation

--- examples/angular-15/rebuild-angular-cli.sh
+++ examples/angular-15/rebuild-angular-cli.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-#pnpm unlink -r
-rm -rf node_modules/@angular-devkit/build-angular node_modules/esbuild
+rm -rf ../../node_modules/@angular-devkit
 
 cd ~/work/angular/angular-cli/
 yarn build
-#cd ~/work/rspack/rspack/examples/angular-15/
+
 cd ~/work/rspack/rspack/
-#pnpm link ~/work/angular/angular-cli/dist/@angular-devkit/build-angular/
 pnpm install
